2bf72436b7c2db205cfbe5966fcd4c289f5269d9
shahin <abd_souk_shahin@yahoo.fr>
2025-06-23 12:13:57 +0200

    update templates

--- default_repo/custom_templates/blocks/data_merger/data_merger.py
+++ default_repo/custom_templates/blocks/data_merger/data_merger.py
@@ -4,7 +4,8 @@ if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
 
-def transform(df_initial, predicted_df_with_metadata, *args, **kwargs):
+@data_exporter
+def data_merger(data, *args, **kwargs):
     """
     Merge predicted data into the initial DataFrame by matching column names.
     Add 'null' for missing columns in the predicted data.
